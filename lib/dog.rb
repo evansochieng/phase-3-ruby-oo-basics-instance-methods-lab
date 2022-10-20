9a1410d80ed629bc603202571cdc42129ea47c20
Evans Ochieng <evansochieng121@gmail.com>
2022-10-20 20:09:10 +0300

    Completed writing instance methods

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+# Create a Dog class and add instances
+class Dog
+    # bark method
+    def bark
+        puts "Woof!"
+    end
+
+    # sit method
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+# Creating instances of class Dog
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark
---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+# class Person
+class Person
+    # talk method
+    def talk
+        puts "Hello World!"
+    end
+
+    #walk method
+    def walk
+        puts "The Person is walking"
+    end
+end
